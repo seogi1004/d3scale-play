29ec829f07f28fd9e6d71e65ce9a011ac0b4206e
alvin.hhh <alvin@jennifersoft.com>
2022-06-08 11:51:52 +0900

    샘플 추가 #radial

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import Linear from './scale/Linear';
 import Power from './scale/Power';
 import Sequential from './scale/Sequential';
 import Diverging from './scale/Diverging';
+import Radial from './scale/Radial';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Diverging />
+      <Radial />
     </div>
   );
 }
--- src/scale/Radial.tsx
+++ src/scale/Radial.tsx
@@ -1,4 +1,5 @@
 import { scaleRadial, scaleBand } from 'd3-scale';
+import type { Pair } from '../types';
 
 const margin = { top: 10, right: 10, bottom: 10, left: 10 };
 const width = 460 - margin.left - margin.right;
@@ -22,12 +23,38 @@ export default function Scale() {
     .range([0, 2 * Math.PI])
     .align(0)
     .domain(data.map((row) => row.name));
-  const y = scaleRadial().domain([innerRadius, outerRadius]).range([0, 1000]);
+  const y = scaleRadial().range([innerRadius, outerRadius]).domain([0, 1000]);
+
+  const newData = data.map((row) => {
+    const angle = x(row.name) || 0;
+
+    return {
+      startAngle: angle,
+      endAngle: angle + x.bandwidth(),
+      outerRadius: y(row.value),
+    };
+  });
+
+  console.log(newData[0]);
+
+  const outerRadius2 = innerRadius + newData[0].outerRadius;
+  const x1 = Math.cos(newData[0].startAngle) * innerRadius;
+  const y1 = Math.sin(newData[0].startAngle) * innerRadius;
+  const x2 = Math.cos(newData[0].endAngle) * innerRadius;
+  const y2 = Math.sin(newData[0].endAngle) * innerRadius;
+  const x3 = Math.cos(newData[0].endAngle) * outerRadius2;
+  const y3 = Math.sin(newData[0].endAngle) * outerRadius2;
+  const x4 = Math.cos(newData[0].startAngle) * outerRadius2;
+  const y4 = Math.sin(newData[0].startAngle) * outerRadius2;
+
+  const points = `${x1},${y1} ${x2},${y2} ${x3},${y3} ${x4},${y4} `;
 
   return (
     <div className="scale">
-      <h1>scaleSequential</h1>
-      <h2></h2>
+      <h1>scaleRadial</h1>
+      <svg width={width} height={height} xmlns="http://www.w3.org/2000/svg">
+        <polygon points={points} />
+      </svg>
     </div>
   );
 }
