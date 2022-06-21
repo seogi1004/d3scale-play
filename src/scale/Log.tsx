619b607162f326bd546c3dac31b6beb4dfe6253f
alvin.hhh <alvin@jennifersoft.com>
2022-06-21 10:38:09 +0900

    Log 스케일 설명 추가

--- src/scale/Log.tsx
+++ src/scale/Log.tsx
@@ -21,6 +21,12 @@ export default function Scale() {
   return (
     <div className="scale">
       <h1>scaleLog</h1>
+      <div className="description">
+        최대값이 극단적으로 클 경우, 결과값의 분포를 비교적 완만하게 보정해주는
+        스케일입니다.
+      </div>
+      <div className="formula">y = mlog(x) + b</div>
+      <pre>const data = [ 10, 100, 10000, 1000000 ];</pre>
       <h3>linear</h3>
       <div className="bar-list">
         {data.map((value, index) => {
